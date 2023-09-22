c31d14e6956f69fbe900a67f2e65ce7437bced18
akabetta <omerr_ylmz@hotmail.com>
2023-09-22 22:04:28 +0300

    details site up up up

--- cafedetails.js
+++ cafedetails.js
@@ -20,6 +20,9 @@ onload = async () => {
     console.log(selectedCafe[0]);
     const cafeImage = document.getElementById('cafeImage');
     cafeImage.src = selectedCafe[3];
+    document.title=selectedCafe[0];
+    const cafeText=document.getElementById('cafeText');
+    cafeText.textContent=selectedCafe[1];
   } catch (error) {
     console.error("Veri yüklenirken bir hata oluştu:", error);
   }
--- cards.js
+++ cards.js
@@ -1,6 +1,6 @@
 const csvData = [];
-const cafes=[];
-
+const cafes = [];
+const cardContainer = document.querySelector(".row");
 
 onload = async () => {
   try {
@@ -8,32 +8,44 @@ onload = async () => {
     const data = await res.text();
     csvData.push(data);
     const rows = csvData[0].split('\r');
-    for (let i=0;i<rows.length;i++){
-      const headers=rows[i].split('|');
+    for (let i = 0; i < rows.length; i++) {
+      const headers = rows[i].split('|');
       cafes.push(headers);
     }
-    cafes.forEach((data) => {
-      const card = createCard(data);
-      cardContainer.appendChild(card);
-    });
+    displayCafes(cafes);
   } catch (error) {
     console.error("Veri yüklenirken bir hata oluştu:", error);
   }
 }
-const cardContainer = document.querySelector(".row");
-  function createCard(data) {
-    const card = document.createElement("div");
-    card.className = "col";
-    card.innerHTML = `
-      <div class="card" style="width: 18rem;">
-        <img src="${data[3]}" class="card-img-top" alt="${data[0]}">
-        <div class="card-body">
-          <h5 class="card-title">${data[0]}</h5>
-          <p class="card-text">${data[1]}</p>
-          <a href="detail.html?id=${data[0]}" class="btn btn-warning">Details</a>
-          </div>
-      </div>
-    `;
-    return card;
-  }
 
+function displayCafes(data) {
+  cardContainer.innerHTML = ''; // Clear the existing cards
+  data.forEach((cafe) => {
+    const card = createCard(cafe);
+    cardContainer.appendChild(card);
+  });
+}
+
+function createCard(data) {
+  const card = document.createElement("div");
+  card.className = "col";
+  card.innerHTML = `
+    <div class="card" style="width: 18rem;">
+      <img src="${data[3]}" class="card-img-top" alt="${data[0]}">
+      <div class="card-body">
+        <h5 class="card-title">${data[0]}</h5>
+        <p class="card-text">${data[1]}</p>
+        <a href="detail.html?id=${data[0]}" class="btn btn-warning">Details</a>
+      </div>
+    </div>
+  `;
+  return card;
+}
+
+const searchInput = document.querySelector("input[type='search']");
+
+searchInput.addEventListener("input", () => {
+  const searchTerm = searchInput.value.toLowerCase();
+  const filteredCafes = cafes.filter((cafe) => cafe[0].toLowerCase().includes(searchTerm));
+  displayCafes(filteredCafes);
+});
